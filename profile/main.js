6bb47526b658fcce4a584a15ed707d0c046712d2
Alejandro Hernández <j.ahr@hotmail.com>
2022-06-29 17:10:53 -0500

    list

--- profile/main.js
+++ profile/main.js
@@ -4,3 +4,15 @@ document.getElementById('btn-close').addEventListener('click', () => {
 document.getElementById('btn-open').addEventListener('click', () => {
     document.getElementById('sidebar').classList.remove('sidebar--active');
 })
+
+let count = 0;
+setInterval(() => {
+    if (count === 5) {
+        document.getElementById('sidebar').classList.remove('sidebar--active');
+    }
+    if (count === 10) {
+        document.getElementById('sidebar').classList.add('sidebar--active');
+    }
+    console.log('aa');
+    count += 1;
+}, 1000)
