612a3b0bf71c99de9c7744343480d6cfc44e573c
Alejandro Hernández <j.ahr@hotmail.com>
2022-07-04 22:14:47 -0500

    modal

--- profile/js/main.js
+++ profile/js/main.js
@@ -5,14 +5,25 @@ document.getElementById('btn-open').addEventListener('click', () => {
     document.getElementById('sidebar').classList.remove('sidebar--active');
 })
 
-// let count = 0;
-// setInterval(() => {
-//     if (count === 5) {
-//         document.getElementById('sidebar').classList.remove('sidebar--active');
-//     }
-//     if (count === 10) {
-//         document.getElementById('sidebar').classList.add('sidebar--active');
-//     }
-//     console.log('aa');
-//     count += 1;
-// }, 1000)
+// modal
+document.getElementById('modal-close').addEventListener('click', () => {
+    document.getElementById('modal').classList.toggle('modal--close');
+    setTimeout(() => {
+        document.querySelectorAll('.modal_content')[0].classList.add('modal_content--hidden');
+    }, 500)
+});
+
+document.querySelectorAll('.modal_content')[0].addEventListener('click', () => {
+    document.getElementById('modal').classList.toggle('modal--close');
+    setTimeout(() => {
+        document.querySelectorAll('.modal_content')[0].classList.add('modal_content--hidden');
+    }, 500)
+});
+
+document.getElementById('open-modal').addEventListener('click', () => {
+    document.querySelectorAll('.modal_content')[0].classList.remove('modal_content--hidden');
+    setTimeout(() => {
+        document.getElementById('modal').classList.remove('modal--close');
+    }, 50)
+});
+
